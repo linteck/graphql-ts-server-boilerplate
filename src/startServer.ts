ff40c75ee4f6c43062f577ce90e53c1f4de89b39
Feng Lin <feng.lin@ubnt>
2020-01-19 20:39:43 +0200

    Can run now.

--- src/startServer.ts
+++ src/startServer.ts
@@ -1,20 +1,20 @@
 import "reflect-metadata";
 import "dotenv/config";
 import { GraphQLServer } from "graphql-yoga";
-import * as session from "express-session";
-import * as connectRedis from "connect-redis";
-import * as RateLimit from "express-rate-limit";
-import * as RateLimitRedisStore from "rate-limit-redis";
+// import * as session from "express-session";
+// import * as connectRedis from "connect-redis";
+// import * as RateLimit from "express-rate-limit";
+// import * as RateLimitRedisStore from "rate-limit-redis";
 
 import { redis } from "./redis";
 import { createTypeormConn } from "./utils/createTypeormConn";
 import { confirmEmail } from "./routes/confirmEmail";
 import { genSchema } from "./utils/genSchema";
-import { redisSessionPrefix } from "./constants";
+// import { redisSessionPrefix } from "./constants";
 import { createTestConn } from "./testUtils/createTestConn";
 
-const SESSION_SECRET = "ajslkjalksjdfkl";
-const RedisStore = connectRedis(session as any);
+// const SESSION_SECRET = "ajslkjalksjdfkl";
+// const RedisStore = connectRedis(session as any);
 
 export const startServer = async () => {
   if (process.env.NODE_ENV === "test") {
@@ -31,34 +31,34 @@ export const startServer = async () => {
     })
   });
 
-  server.express.use(
-    new RateLimit({
-      store: new RateLimitRedisStore({
-        client: redis
-      }),
-      windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 100, // limit each IP to 100 requests per windowMs
-      delayMs: 0 // disable delaying - full speed until the max limit is reached
-    })
-  );
-
-  server.express.use(
-    session({
-      store: new RedisStore({
-        client: redis as any,
-        prefix: redisSessionPrefix
-      }),
-      name: "qid",
-      secret: SESSION_SECRET,
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
-      }
-    } as any)
-  );
+  // server.express.use(
+  //   new RateLimit({
+  //     store: new RateLimitRedisStore({
+  //       client: redis
+  //     }),
+  //     windowMs: 15 * 60 * 1000, // 15 minutes
+  //     max: 100, // limit each IP to 100 requests per windowMs
+  //     delayMs: 0 // disable delaying - full speed until the max limit is reached
+  //   })
+  // );
+  //
+  // server.express.use(
+  //   session({
+  //     store: new RedisStore({
+  //       client: redis as any,
+  //       prefix: redisSessionPrefix
+  //     }),
+  //     name: "qid",
+  //     secret: SESSION_SECRET,
+  //     resave: false,
+  //     saveUninitialized: false,
+  //     cookie: {
+  //       httpOnly: true,
+  //       secure: process.env.NODE_ENV === "production",
+  //       maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
+  //     }
+  //   } as any)
+  // );
 
   const cors = {
     credentials: true,
