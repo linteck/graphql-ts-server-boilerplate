42368d1ea1360877e6ce4449dec59a137bbb52a5
Feng Lin <feng.lin@ubnt>
2020-01-19 21:54:44 +0200

    Disable redis.

--- src/startServer.ts
+++ src/startServer.ts
@@ -6,7 +6,7 @@ import { GraphQLServer } from "graphql-yoga";
 // import * as RateLimit from "express-rate-limit";
 // import * as RateLimitRedisStore from "rate-limit-redis";
 
-import { redis } from "./redis";
+//import { redis } from "./redis";
 import { createTypeormConn } from "./utils/createTypeormConn";
 import { confirmEmail } from "./routes/confirmEmail";
 import { genSchema } from "./utils/genSchema";
@@ -18,13 +18,13 @@ import { createTestConn } from "./testUtils/createTestConn";
 
 export const startServer = async () => {
   if (process.env.NODE_ENV === "test") {
-    await redis.flushall();
+    //await redis.flushall();
   }
 
   const server = new GraphQLServer({
     schema: genSchema() as any,
     context: ({ request }) => ({
-      redis,
+      //redis,
       url: request.protocol + "://" + request.get("host"),
       session: request.session,
       req: request
